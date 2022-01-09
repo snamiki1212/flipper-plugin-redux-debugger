e7613562c0f96b1bd906c0df5b9345de783c5eb2
snamiki1212 <snamiki1212@gmail.com>
2022-01-09 09:36:34 -0800

    refactor: organize logics

--- renderAction.tsx
+++ renderAction.tsx
@@ -10,6 +10,57 @@ export const renderAction = (action: string) => {
   return <>{component}</>;
 };
 
+/**
+ * Constants
+ */
+const SEPARATOR = "/";
+const RTK_THUNK_LIST = ["pending", "fulfilled", "rejected"]; // NOTE: https://redux-toolkit.js.org/api/createAsyncThunk#type
+const COLOR_PALETTE = ["#ec5f67", "#E0AF02", "#0c969b", "#994cc3"];
+const SEPARATOR_COLOR = "#CCD1E4";
+const SPECIAL_TEXT_COLOR = "#CCD1E4";
+
+/**
+ * Types
+ */
+type Category = {
+  text: string;
+  isSeparator?: boolean;
+  isSpecialText?: boolean;
+  color?: string;
+};
+
+/**
+ * Helpers
+ */
+const split = (text: string) => {
+  let parts = text.split(SEPARATOR);
+  for (let i = parts.length; i-- > 1; ) parts.splice(i, 0, SEPARATOR);
+  return parts;
+};
+
+const categorize = (list: string[]): Category[] => {
+  return list.map((text) => {
+    if (text === SEPARATOR) return { text, isSeparator: true };
+    if (RTK_THUNK_LIST.includes(text)) return { text, isSpecialText: true };
+    return { text };
+  });
+};
+
+const colorize = (list: Category[]) => {
+  let i = 0;
+  return list.map((item) => {
+    if (item.isSeparator) return { ...item, color: SEPARATOR_COLOR };
+    if (item.isSpecialText) return { ...item, color: SPECIAL_TEXT_COLOR };
+    const obj = { ...item, color: COLOR_PALETTE[i] };
+    if (i === COLOR_PALETTE.length - 1) {
+      i = 0;
+    } else {
+      i += 1;
+    }
+    return obj;
+  });
+};
+
 const componentrize = (list: Category[]) => {
   const components = list.map(({ text, isSeparator, isSpecialText, color }) => {
     const paddingHorizontal = isSeparator
@@ -26,47 +77,4 @@ const componentrize = (list: Category[]) => {
     );
   });
   return <>{components}</>;
-};
-
-const SEPARATOR = "/";
-const split = (text: string) => {
-  let parts = text.split(SEPARATOR);
-  for (let i = parts.length; i-- > 1; ) parts.splice(i, 0, SEPARATOR);
-  return parts;
-};
-
-const RTK_THUNK_LIST = ["pending", "fulfilled", "rejected"]; // NOTE: https://redux-toolkit.js.org/api/createAsyncThunk#type
-
-type Category = {
-  text: string;
-  isSeparator?: boolean;
-  isSpecialText?: boolean;
-  color?: string;
-};
-
-const categorize = (list: string[]): Category[] => {
-  return list.map((text) => {
-    if (text === SEPARATOR) return { text, isSeparator: true };
-    if (RTK_THUNK_LIST.includes(text)) return { text, isSpecialText: true };
-    return { text };
-  });
-};
-
-const COLOR_PALETTE = ["#ec5f67", "#E0AF02", "#0c969b", "#994cc3"];
-const SEPARATOR_COLOR = "#CCD1E4";
-const SPECIAL_TEXT_COLOR = "#CCD1E4";
-
-const colorize = (list: Category[]) => {
-  let i = 0;
-  return list.map((item) => {
-    if (item.isSeparator) return { ...item, color: SEPARATOR_COLOR };
-    if (item.isSpecialText) return { ...item, color: SPECIAL_TEXT_COLOR };
-    const obj = { ...item, color: COLOR_PALETTE[i] };
-    if (i === COLOR_PALETTE.length - 1) {
-      i = 0;
-    } else {
-      i += 1;
-    }
-    return obj;
-  });
 };
